c13f02ee10bffbe21e09d581347ce8c99ad75688
xiangshaopeng08028 <xiangshaopeng08028@hellobike.com>
2020-10-22 17:32:24 +0800

    leetcode19

--- src/main/java/com/cherry/algorithm/list/Leetcode_19.java
+++ src/main/java/com/cherry/algorithm/list/Leetcode_19.java
@@ -3,27 +3,6 @@ package com.cherry.algorithm.list;
 import com.cherry.algorithm.utils.ListNode;
 
 public class Leetcode_19 {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode slow = head;
-        ListNode quick = head;
-        int count = 0;
-        while (count < n) {
-            quick = quick.next;
-            count++;
-        }
-
-        if (quick == null) {
-            return slow.next;
-        }
-
-        while (quick.next != null) {
-            slow = slow.next;
-            quick = quick.next;
-        }
-
-        slow.next = slow.next.next;
-        return head;
-    }
 
     /**
      * 哨兵 + 快慢指针
@@ -31,22 +10,19 @@ public class Leetcode_19 {
      * @param n
      * @return
      */
-    public ListNode removeNthFromEnd2(ListNode head, int n) {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode sentry = new ListNode(-1);
         sentry.next = head;
-        ListNode quick = sentry;
         ListNode slow = sentry;
-
-        int count = 0;
-        while (count < n) {
+        ListNode quick = sentry;
+        while (n > 0) {
             quick = quick.next;
-            count++;
+            n--;
         }
         while (quick.next != null) {
-            slow = slow.next;
             quick = quick.next;
+            slow = slow.next;
         }
-
         slow.next = slow.next.next;
         return sentry.next;
     }
