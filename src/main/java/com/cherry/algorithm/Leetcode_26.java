923cced34d7ec6c5fd6f18eacf9d3eaba2aedb2e
pick_cherry <515811019@qq.com>
2022-02-12 21:24:04 +0800

    lc26

---
+++ src/main/java/com/cherry/algorithm/Leetcode_26.java
@@ -0,0 +1,19 @@
+package com.cherry.algorithm;
+
+public class Leetcode_26 {
+    /**
+     * 快慢双指针
+     */
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            int slow = 0, quick = 0;
+            for (; quick < nums.length - 1; quick++) {
+                if (nums[quick] != nums[quick + 1]) {
+                    nums[++slow] = nums[quick + 1];
+                }
+            }
+            return slow + 1;
+
+        }
+    }
+}
