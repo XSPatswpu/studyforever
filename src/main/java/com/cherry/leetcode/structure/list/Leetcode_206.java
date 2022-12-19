95dc0462df6facd66d9fed3a2241310c989e1ca0
pick_cherry <515811019@qq.com>
2022-12-19 21:37:01 +0800

    反转链表递归实现

--- src/main/java/com/cherry/leetcode/structure/list/Leetcode_206.java
+++ src/main/java/com/cherry/leetcode/structure/list/Leetcode_206.java
@@ -16,6 +16,9 @@ public class Leetcode_206 {
      * 所以在做这个操作之前，要想办法把 当前节点的 next 节点存下来。
      * 又由于我要让 currentNode.next = previous，而 previous 我如果不储存的话，我压根儿就找不到，所以又需要一个指针储存 previous
      *
+     * 核心思路：
+     * 1. 用两个指针完成交换，然后迭代向后移动
+     * 2. 交换的时候，考虑先临时储存然后再交换
      * @param head
      * @return
      */
@@ -44,6 +47,24 @@ public class Leetcode_206 {
         head.next.next = head;
         head.next = null;
         return listNode;
+    }
+
+    /**
+     * 同样是递归，但是这种方案仿佛能看到迭代的影子
+     * @param head
+     * @return
+     */
+    public ListNode reverseList1(ListNode head) {
+        return doReverse(null, head);
+    }
+
+    public ListNode doReverse(ListNode prev, ListNode current) {
+        if (current == null) {
+            return prev;
+        }
+        ListNode temp = current.next;
+        current.next = prev;
+        return doReverse(current, temp);
     }
 
 
