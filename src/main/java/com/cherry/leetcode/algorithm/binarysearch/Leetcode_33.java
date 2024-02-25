9554008b9b9f1a8199ae1eb67ef287ec76cae895
cherry-peng <xspatswpu@163.com>
2024-02-25 18:27:10 +0800

    20240225-lc33

--- src/main/java/com/cherry/leetcode/algorithm/binarysearch/Leetcode_33.java
+++ src/main/java/com/cherry/leetcode/algorithm/binarysearch/Leetcode_33.java
@@ -94,5 +94,35 @@ public class Leetcode_33 {
             }
             return -1;
         }
+
+
+        public int search2(int[] nums, int target) {
+
+            int l = 0;
+            int r = nums.length - 1;
+
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[l] <= nums[mid]) {
+                    if (nums[l] <= target && nums[mid] >= target) {
+                        r = mid - 1;
+                    } else {
+                        l = mid + 1;
+                    }
+                } else {
+                    if (nums[mid] <= target && nums[r] >= target) {
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+
+            }
+
+            return -1;
+        }
     }
 }
