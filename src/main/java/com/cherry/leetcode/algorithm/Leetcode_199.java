08e5bc7babd0660a38d77c3f787438b64ef97a0a
cherry-peng <xspatswpu@163.com>
2024-03-11 22:57:16 +0800

    20240311

---
+++ src/main/java/com/cherry/leetcode/algorithm/Leetcode_199.java
@@ -0,0 +1,39 @@
+package com.cherry.leetcode.algorithm;
+
+import com.cherry.leetcode.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Leetcode_199 {
+
+    class Solution {
+        public List<Integer> rightSideView(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            if (root == null) {
+                return list;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                int n = queue.size();
+                for (int i = 0; i < n; i++) {
+                    TreeNode node = queue.poll();
+                    if (node.right != null) {
+                        queue.add(node.right);
+                    }
+                    if (node.left != null) {
+                        queue.add(node.left);
+                    }
+                    if (i == 0) {
+                        list.add(node.val);
+                    }
+                }
+            }
+            return list;
+        }
+    }
+
+}
