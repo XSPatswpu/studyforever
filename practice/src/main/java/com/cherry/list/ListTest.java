e5d68584f9cdfd30757ee2352ad94f0ac35c042d
cherry-peng <xspatswpu@gmail.com>
2025-06-17 16:09:26 +0800

    增加日常测试模块

---
+++ practice/src/main/java/com/cherry/list/ListTest.java
@@ -0,0 +1,18 @@
+package com.cherry.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+    public static void main(String[] args) {
+        List<String> l = new ArrayList<>();
+        l.add("1");
+        l.add("2");
+        l.add("3");
+        l.add("4");
+
+        List<String> list = l.subList(0, 1);
+        System.out.println(list);
+    }
+
+}
