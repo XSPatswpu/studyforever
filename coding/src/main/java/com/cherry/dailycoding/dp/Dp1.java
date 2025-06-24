2b359864d781e8c60bca4d8609066ea22e3c4ee7
cherry-peng <xspatswpu@gmail.com>
2025-06-24 10:24:04 +0800

    dp 70 & 509

---
+++ coding/src/main/java/com/cherry/dailycoding/dp/Dp1.java
@@ -0,0 +1,47 @@
+package com.cherry.dailycoding.dp;
+
+public class Dp1 {
+    // lc 509
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    /**
+     * 1. 确定 dp 数据及下标的含义
+     * 3. 寻找递推公式
+     * 2. 初始化 dp[0] dp[1]
+     * 4. 确定推导顺序
+     * 5. 举例推导
+     *
+     */
+    public int fib2(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public int fib3(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int a = 0;
+        int b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+
+}
---
+++ coding/src/main/java/com/cherry/dailycoding/dp/Dp2.java
@@ -0,0 +1,23 @@
+package com.cherry.dailycoding.dp;
+
+public class Dp2 {
+    // lc 70
+
+    /**
+     * n > 0
+     * 1. 定义 dp 数组：dp[n]
+     * 2. 寻找递推公式：定义dp[n] = dp[n - 1] + dp[n - 2]
+     * 3. 初始化递推公式的依赖的边界值：dp[1] = 1 dp[2] = 2
+     * 4. 确定递推顺序
+     * 5. 举例推导 dp 数组
+     */
+    public int func(int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n ; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
+    }
+}
